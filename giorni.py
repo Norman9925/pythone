8dec38e4d7961817e53b9ded8e469d1c18ae1f03
Norman9925 <norman.muzi99@gmail.com>
2025-03-18 16:18:41 +0100

    Create giorni.py

---
+++ giorni.py
@@ -0,0 +1,10 @@
+GIORNI = ("Lunedì   " +  
+          "Martedì  " +
+          "Mercoledì" +
+          "Giovedì  " +
+          "Venerdì  " +
+          "Sabato   " +
+          "Domenica ") 
+  giorno = int(input("Indica giorno: "))
+ p = (giorno - 1) * 7
+print(GIORNI[p], GIORNI[p1], GIORNI[p2], GIORNI[p3], GIORNI[p4], GIORNI[p5], GIORNI[p6], GIORNI[p7])
