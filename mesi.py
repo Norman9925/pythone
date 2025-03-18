c2bfead8dd34999ce2eb75a2299efd2d4be28a81
Norman9925 <norman.muzi99@gmail.com>
2025-03-18 16:01:04 +0100

    Create mesi.py

---
+++ mesi.py
@@ -0,0 +1,15 @@
+MESI = ("Gennaio  " +  
+        "Febbraio " +
+        "Marzo    " +
+        "Aprile   " +
+        "Maggio   " +
+        "Giugno   " +
+        "Luglio   " +
+        "Agosto   " +
+        "Settembre" +
+        "Ottobre  " + 
+        "Novembre " +
+        "Dicembre ") 
+  mese = int(input("Indica mese: "))
+ p = (mese - 1) * 9
+print(MESI[p], MESI[p1], MESI[p2], MESI[p3], MESI[p4], MESI[p5], MESI[p6], MESI[p7], MESI[p8], MESI[p9])
